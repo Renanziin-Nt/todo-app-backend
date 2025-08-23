49b1fb65d7e829f9e7d6e7e4309948b9755d5325
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2025-08-23 10:59:22 -0300

    feat: create prisma module and update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
+
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, AuthModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
