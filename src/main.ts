5501ebdc4ab2dedd159a0c95228d1fc4a47acff6
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2025-08-26 11:06:51 -0300

    feat: Define server cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     transform: true,
   }));
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization, Accept',
     credentials: true,
